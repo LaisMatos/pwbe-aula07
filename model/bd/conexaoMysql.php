fffcdb2a6353f9d44b744fc6c9805927a755fec2
LAIS MATOS DE CARVALHO <21276360@CFP127ALUNO.LOCAL>
2022-02-25 16:26:17 -0300

    conexao com banco de dados

---
+++ model/bd/conexaoMysql.php
@@ -0,0 +1,68 @@
+<?php
+
+/******************************************** 
+obj: arq para criar conexão com banco MySql
+autor: lais
+Data: 25.02.22
+versão: 1.0
+********************************************/
+
+
+// const par estabelecer a conexão com o BD 
+const SERVER = 'localhost'; // local do banco
+const USER = 'root'; //usuário
+const PASSWORD ='bcd127';  //senha
+const DATABASE = 'dbContatos'; //nome do banco
+
+//criar variavel $resultado que recebe a função conexaoMysql() para executar a função e exibir via var_dump o código
+$resultado = conexaoMysql();
+var_dump($resultado);
+
+// fun para abertura da conexão com bd
+function conexaoMysql(){
+    
+    //criação de array para armazenar dados do banco
+    $conexao=array();
+    
+    //se conexão for stabelecida com o BD, retornará um array de dados sobre a conexão
+    $conexao=mysqli_connect(SERVER, USER, PASSWORD, DATABASE);
+
+    //teste lógico para validar se a conexão foi realizada com sucesso
+    if ($conexao) {
+        return $conexao;
+    }else {
+        return false;
+    }
+
+}
+
+/*########### ANOTAÇÕES ##############
+
+Formas de conectar-se com o BD MySql
+    mysql_connet() : versão antiga do php para fazer a conexão com o BD (ñ tem performance e segurança)
+    mysqli_connet() : versão mais atual do php para fazer a conexão com o BD (pode ser usada para programação estruturada e poo)
+    PDO() : versão recente, mais completa e eficiente para conectar-se com o BD (indicada pela segurança e poo)
+
+Roteiro para estabelecer conexão com banco de dados
+    -criar função  
+    -escolher biblioteca 
+    -especificar para biblioteca local do banco, usuário, senha e nome do banco
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
